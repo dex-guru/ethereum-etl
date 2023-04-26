cacc52b78e1d5a0fbbb27726aa7ffe52983be15f
Sergey V <q>
2023-04-26 14:35:39 +0600

    refactor: clickhouse exporter insert

--- blockchainetl/jobs/exporters/clickhouse_exporter.py
+++ blockchainetl/jobs/exporters/clickhouse_exporter.py
@@ -92,62 +92,7 @@ class ClickHouseItemExporter:
                     )
                     column_names = self.tables[table].column_names
                     column_types = self.tables[table].column_types
-                    try:
-                        self.connection.insert(
-                            table,
-                            data=table_data,
-                            column_names=column_names,
-                            column_types=column_types,
-                            database=self.database,
-                        )
-                    except clickhouse_connect.driver.exceptions.ProgrammingError as e:
-                        for row in table_data:
-                            for i, column_value in enumerate(row):
-                                column_type = column_types[i]
-                                column_name = column_names[i]
-                                if isinstance(column_value, int):
-                                    max_value = NUMERIC_TYPE_MAX_VALUES.get(type(column_type))
-                                    if max_value is None:
-                                        raise
-
-                                    if column_value > max_value:
-                                        logging.warning(
-                                            "Insert error: too large column value:"
-                                            " table=%s column=%s column_type=%s"
-                                            " column_value=%s row_data_json=%s",
-                                            table,
-                                            column_name,
-                                            column_type.__class__.__name__,
-                                            column_value,
-                                            json.dumps(
-                                                {
-                                                    name: value
-                                                    for name, value in zip(column_names, row)
-                                                }
-                                            ),
-                                        )
-                                        raise OverflowError(
-                                            f"Too large column value: table={table} column={column_name}"
-                                        ) from e
-                                elif column_value is None and not column_type.nullable:
-                                    logging.error(
-                                        "Insert error: cannot insert null value into non-nullable column:"
-                                        " table=%s column=%s column_type=%s row_data_json=%s",
-                                        table,
-                                        column_name,
-                                        column_type.__class__.__name__,
-                                        json.dumps(
-                                            {
-                                                name: value
-                                                for name, value in zip(column_names, row)
-                                            }
-                                        ),
-                                    )
-                                    raise TypeError(
-                                        "Cannot insert null value into non-nullable column:"
-                                        f" table={table} column={column_name}"
-                                    ) from e
-                        raise
+                    self._insert(column_names, column_types, table, table_data)
                     self.cached_batches[table] = []
                 else:
                     # insufficient size, so cache
@@ -157,6 +102,64 @@ class ClickHouseItemExporter:
                         )
                     )
                     self.cached_batches[table] = batch
+
+    def _insert(self, column_names, column_types, table, table_data):
+        try:
+            self.connection.insert(
+                table,
+                data=table_data,
+                column_names=column_names,
+                column_types=column_types,
+                database=self.database,
+            )
+        except clickhouse_connect.driver.exceptions.ProgrammingError as e:
+            for row in table_data:
+                for i, column_value in enumerate(row):
+                    column_type = column_types[i]
+                    column_name = column_names[i]
+                    if isinstance(column_value, int):
+                        max_value = NUMERIC_TYPE_MAX_VALUES.get(type(column_type))
+                        if max_value is None:
+                            raise
+
+                        if column_value > max_value:
+                            logging.warning(
+                                "Insert error: too large column value:"
+                                " table=%s column=%s column_type=%s"
+                                " column_value=%s row_data_json=%s",
+                                table,
+                                column_name,
+                                column_type.__class__.__name__,
+                                column_value,
+                                json.dumps(
+                                    {
+                                        name: value
+                                        for name, value in zip(column_names, row)
+                                    }
+                                ),
+                            )
+                            raise OverflowError(
+                                f"Too large column value: table={table} column={column_name}"
+                            ) from e
+                    elif column_value is None and not column_type.nullable:
+                        logging.error(
+                            "Insert error: cannot insert null value into non-nullable column:"
+                            " table=%s column=%s column_type=%s row_data_json=%s",
+                            table,
+                            column_name,
+                            column_type.__class__.__name__,
+                            json.dumps(
+                                {
+                                    name: value
+                                    for name, value in zip(column_names, row)
+                                }
+                            ),
+                        )
+                        raise TypeError(
+                            "Cannot insert null value into non-nullable column:"
+                            f" table={table} column={column_name}"
+                        ) from e
+            raise
 
     def convert_items(self, items):
         for item in items:
@@ -171,8 +174,7 @@ class ClickHouseItemExporter:
         # clear the cache
         logging.info("Flushing remaining batches")
         for table, batch in self.cached_batches.items():
-            self.connection.insert(table, data=batch, column_names=self.tables[table].column_names,
-                                   column_types=self.tables[table].column_types, database=self.database)
+            self._insert(self.tables[table].column_names, self.tables[table].column_types, table, batch)
         self.connection.close()
 
     def group_items_by_table(self, items):
