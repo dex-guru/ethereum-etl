8d31c07376f9c4ba1fe3f619a778002ab2030455
Sergey V <q@q.q>
2023-08-01 14:46:10 +0600

    refactor: test_clickhouse

--- tests/ethereumetl/test_clickhouse.py
+++ tests/ethereumetl/test_clickhouse.py
@@ -1,4 +1,7 @@
 import json
+from collections.abc import Sequence
+
+from clickhouse_connect.driver.client import Client
 
 from blockchainetl.jobs.exporters.clickhouse_exporter import ClickHouseItemExporter
 from ethereumetl.scripts.execute_clickhouse_sql import execute_clickhouse_sql
@@ -17,6 +20,12 @@ def create_entity_tables(clickhouse_url):
         ch_exporter.create_tables()
     finally:
         ch_exporter.close()
+
+
+def insert_records(clickhouse: Client, table: str, records: Sequence[dict]):
+    columns = records[0].keys()
+    data = [[r[col] for col in columns] for r in records]
+    clickhouse.insert(table, data, columns)
 
 
 def test_events_mat_view(clickhouse_url, clickhouse):
@@ -55,7 +64,6 @@ def test_events_mat_view(clickhouse_url, clickhouse):
             ),
         },
     ]
-    event_infos_column_names = event_infos[0].keys()
 
     logs = [
         {
@@ -78,14 +86,9 @@ def test_events_mat_view(clickhouse_url, clickhouse):
             'block_hash': '0x2aee15c56ee9b2bccbfccbfe62d6cebcb2af7bf736ab59259804398f502d7fe7',
         },
     ]
-    logs_column_names = logs[0].keys()
 
     # Add some event infos to be captured my the MaterializedView
-    clickhouse.insert(
-        '1_event_inventory_src',
-        [[record[key] for key in event_infos_column_names] for record in event_infos],
-        event_infos_column_names,
-    )
+    insert_records(clickhouse, '1_event_inventory_src', event_infos)
 
     # 1_event_inventory_src
     assert list(clickhouse.query('SELECT * FROM 1_event_inventory_src').named_results()) == [
@@ -114,11 +117,7 @@ def test_events_mat_view(clickhouse_url, clickhouse):
     ]
 
     # Insert logs. This should join with event_infos and insert into 1_events
-    clickhouse.insert(
-        '1_logs',
-        [[record[key] for key in logs_column_names] for record in logs],
-        logs_column_names,
-    )
+    insert_records(clickhouse, '1_logs', logs)
 
     events_records = list(
         clickhouse.query(
@@ -163,29 +162,33 @@ def test_load_abis_to_event_inventory(clickhouse_url, clickhouse, tmp_path):
         dry_run=False,
     )
 
-    event_abi = {
-        'anonymous': False,
-        'inputs': [
-            {'indexed': True, 'internalType': 'address', 'name': 'operator', 'type': 'address'},
-            {'indexed': True, 'internalType': 'address', 'name': 'from', 'type': 'address'},
-            {'indexed': True, 'internalType': 'address', 'name': 'to', 'type': 'address'},
-            {'indexed': False, 'internalType': 'uint256', 'name': 'id', 'type': 'uint256'},
-            {'indexed': False, 'internalType': 'uint256', 'name': 'value', 'type': 'uint256'},
-        ],
-        'name': 'TransferSingle',
-        'type': 'event',
-    }
+    abi = [
+        {
+            'anonymous': False,
+            'inputs': [
+                {
+                    'indexed': True,
+                    'internalType': 'address',
+                    'name': 'operator',
+                    'type': 'address',
+                },
+                {'indexed': True, 'internalType': 'address', 'name': 'from', 'type': 'address'},
+                {'indexed': True, 'internalType': 'address', 'name': 'to', 'type': 'address'},
+                {'indexed': False, 'internalType': 'uint256', 'name': 'id', 'type': 'uint256'},
+                {'indexed': False, 'internalType': 'uint256', 'name': 'value', 'type': 'uint256'},
+            ],
+            'name': 'TransferSingle',
+            'type': 'event',
+        },
+    ]
     file_name1 = 'abi1.json'
     file_name2 = 'abi2.json'
-    subdir_name = 'subdir'
-    subdir_path = tmp_path / subdir_name
+    subdir_path = tmp_path / 'subdir'
     subdir_path.mkdir()
-    full_file_path1 = tmp_path / subdir_name / file_name1
-    full_file_path2 = tmp_path / subdir_name / file_name2
-    json.dump([event_abi], full_file_path1.open('w'))
-    json.dump([event_abi], full_file_path2.open('w'))
+    (subdir_path / file_name1).write_text(json.dumps(abi))
+    (subdir_path / file_name2).write_text(json.dumps(abi))
 
-    load_abis_to_event_inventory(1, clickhouse_url, str(tmp_path.resolve()), dry_run=False)
+    load_abis_to_event_inventory(1, clickhouse_url, str(tmp_path), dry_run=False)
 
     event_inventory_records = list(
         clickhouse.query(
@@ -199,7 +202,7 @@ def test_load_abis_to_event_inventory(clickhouse_url, clickhouse, tmp_path):
                 '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62',
                 4,
             ),
-            'event_abi_json': json.dumps(event_abi, separators=(',', ':')),
+            'event_abi_json': json.dumps(abi[0], separators=(',', ':')),
             'event_name': 'TransferSingle',
             'event_signature': 'TransferSingle(address,address,address,uint256,uint256)',
             'event_signature_hash': '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62',
