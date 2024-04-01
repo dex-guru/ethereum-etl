9a723974a33db0a39ea530ada282feeaf47d9841
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2024-04-02 00:18:58 +0400

    fix

--- ethereumetl/service/price_service.py
+++ ethereumetl/service/price_service.py
@@ -87,10 +87,10 @@ class PriceService:
             )
             return dex_trade
 
-        for token_address, current_price in zip(
-            dex_trade['token_addresses'], dex_trade['prices_stable']
-        ):
-            for price_type in ('stable', 'native'):
+        for price_type in ('stable', 'native'):
+            for token_address, current_price in zip(
+                dex_trade['token_addresses'], dex_trade[f'prices_{price_type}']
+            ):
                 if not is_valid_with_previous_price(token_address, current_price, price_type):
                     reset_prices()
                     return dex_trade
