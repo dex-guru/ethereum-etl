acfa1a89d21dd55885cdb507e9f0f942b5c15468
Tiago Silva <tfn.da.silva@gmail.com>
2025-03-11 16:31:15 +0000

    Output trade token_address as source and target token, update get scalar and add dex pool info, clean-up

--- ethereumetl/service/dex/carbondefi/carbondefi.py
+++ ethereumetl/service/dex/carbondefi/carbondefi.py
@@ -26,10 +26,11 @@ class CarbonTransactionType(Enum):
 
 
 class CarbonDeFiAmm(BaseDexClient):
+
     def __init__(self, web3: Web3, chain_id: int | None = None, file_path: str = __file__):
         super().__init__(web3, chain_id, file_path)
         carbon_controller_abi_path = Path(file_path).parent / "CarbonController.json"
-        self.carbon_controller_abi = self._initiate_contract(abi_path=carbon_controller_abi_path)
+        self.carbon_controller = self._initiate_contract(abi_path=carbon_controller_abi_path)
 
         with open(Path(__file__).parent / "deploys" / str(chain_id) / "metadata.json") as f:
             metadata = json.load(f)
@@ -38,14 +39,13 @@ class CarbonDeFiAmm(BaseDexClient):
     @lru_cache(maxsize=128)
     def _get_pair_fee_ppm(self, token0: ChecksumAddress, token1: ChecksumAddress) -> int | None:
         try:
-            pair_fee_ppm = self.carbon_controller_abi.functions.pairTradingFeePPM(
+            pair_fee_ppm = self.carbon_controller.functions.pairTradingFeePPM(
                 to_checksum(token0), to_checksum(token1)
             ).call({"to": self.CARBON_CONTROLLER}, "latest")
         except (ValueError, TypeError, BadFunctionCallOutput, ContractLogicError):
             return None
         return int(pair_fee_ppm)
 
-    @lru_cache(maxsize=128)
     def _get_token_reserves(
         self, token0: ChecksumAddress, token1: ChecksumAddress
     ) -> list[float] | None:
@@ -72,17 +72,31 @@ class CarbonDeFiAmm(BaseDexClient):
                 if not token_in or not token_out or not fee:
                     return None
 
-                pairs = self.carbon_controller_abi.functions.pairs()
+                pairs = self.carbon_controller.functions.pairs()
                 unique_tokens = {token for pair in pairs for token in pair}
 
                 return EthDexPool(
                     address=self.CARBON_CONTROLLER,
+                    factory_address=self.CARBON_CONTROLLER,
                     token_addresses=unique_tokens,
+                    lp_token_addresses=[self.CARBON_CONTROLLER],
                     fee=fee,
                 )
             except (ValueError, TypeError, KeyError) as e:
                 logs.debug(f"Resolved asset not found for ${parsed_event}: {e}")
             return None
+
+    @staticmethod
+    def _get_scalar_for_token(tokens_for_pool: list[EthToken], token_addresses: str) -> list[int]:
+        token_scalars = []
+        for token_address in token_addresses:
+            token = next(
+                (t for t in tokens_for_pool if t.address.lower() == token_address.lower()), None
+            )
+            if not token:
+                return []
+            token_scalars.append(10**token.decimals)
+        return token_scalars
 
     def resolve_receipt_log(
         self,
@@ -106,7 +120,9 @@ class CarbonDeFiAmm(BaseDexClient):
                 if not source_amount or not target_amount:
                     return None
 
-                token_scalars = self._get_scalars_for_tokens(tokens_for_pool, dex_pool)
+                token_scalars = self._get_scalar_for_token(
+                    tokens_for_pool, [source_token, target_token]
+                )
 
                 token_reserves = self._get_token_reserves(source_token, target_token)
                 if not token_reserves:
@@ -133,7 +149,7 @@ class CarbonDeFiAmm(BaseDexClient):
                     event_type='swap',
                     token_reserves=token_reserves,
                     token_prices=get_prices_for_two_pool(prices[0], prices[1]),
-                    token_addresses=dex_pool.token_addresses,
+                    token_addresses=[source_token, target_token],
                 )
             except (ValueError, TypeError, KeyError) as e:
                 logs.debug(f"Resolved log not found for ${parsed_receipt_log}: {e}")
